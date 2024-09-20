e27eb7caded1f69c937bd8e48c2b9052b7bc35eb
haripriya234 <132989133+haripriya234@users.noreply.github.com>
2024-09-20 13:07:50 +0530

    Add files via upload

---
+++ createFile.java
@@ -0,0 +1,24 @@
+import java.io.*;
+class createFile{
+    public static void main(String[] args){
+        //create folder
+        // File f=new File("boo");
+        // System.out.println(f.mkdir());
+        // System.out.println(f.exists());
+        //Create file       
+        // File f1=new File("C:\\Users\\Lenovo\\OneDrive\\Desktop\\he\\FileHandling\\boo\\boo.txt");
+        // try{
+        //     f1.createNewFile();
+        // }
+        // catch(IOException e){
+        //     e.printStackTrace();
+        // }
+        // //RENAME
+        // File f1=new File("C:\\Users\\Lenovo\\OneDrive\\Desktop\\he\\FileHandling\\boo\\boo.txt");
+        // File f2=new File("C:\\Users\\Lenovo\\OneDrive\\Desktop\\he\\FileHandling\\boo\\boot.txt");
+        // f1.renameTo(f2);
+        // //DELETE
+        // File f1=new File("C:\\Users\\Lenovo\\OneDrive\\Desktop\\he\\FileHandling\\boo\\boot.txt");
+        // f1.delete();
+    }
+}
